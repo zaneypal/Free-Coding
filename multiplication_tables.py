7759cb3452db05715057ee3844d2fd90f496c904
zaneypal <101521294+zaneypal@users.noreply.github.com>
2023-02-16 10:59:46 -0500

    Added hearts icons so it's cuter now Other icons added: - Correct symbol - X for wrong symbol
    Fixed a bug where you couldn't exit out of a question
    if you didn't answer with 'q' or a number the first time.
    
    There is now a score variable where you can track your score.
    The score doesn't reset if you get a question wrong, unlike the streak.

--- multiplication_tables.py
+++ multiplication_tables.py
@@ -13,11 +13,18 @@ import sys
 def tab(amount):
     return "\t"*amount
 
+def hearts_count(amount):
+    lost = 3 - amount
+    print("Hearts remaining: " + "♥ "*amount + "♡ "*lost)
+    print("")
+
 # ak means Accepted Keywords
 view_tables_ak = ["view tables", "view table", "vt", "v", "vi", "view", "tables", "table"]
 quiz_ak = ["quiz", "qu", "qui", "quiz time"]
-quiz_mode1_ak = ["mode1", "1", "m1", "md1", "first mode", "first", "1st"]
-quiz_mode2_ak = ["mode2", "2", "m2", "md2", "second mode", "second", "2nd"]
+quiz_mode1_ak = ["mode1", "mode 1", "mod1", "mo1", "1", "m1", "md1", "first mode", "first", "1st"]
+quiz_mode2_ak = ["mode2", "mode 2", "mod2", "mo2", "2", "m2", "md2", "second mode", "second", "2nd"]
+yes_ak = ["y", "yes", "yea", "yay", "okay", "ok", "yeah", "yep", "mhm", "ya", "yah"]
+no_ak = ["n", "no", "nah", "nay", "nope", "noo", "nooo", "nein", "non", "no way"]
 
 print("\n\nWelcome to The Multiplication Tables Program")
 
@@ -25,9 +32,7 @@ while start_mode == True:
     view_tables = False
     quiz = False
     answer_mode = True
-    streak = 0
-    hearts = 3
-    mode = input(f"\n{tab(5)}      Available Modes:\n{tab(6)}VIEW TABLES\n{tab(6)}   QUIZ\n\nChoose a mode:  ")
+    mode = input(f"\n{tab(5)}      Available Modes:\n{tab(6)}\033[1mVIEW TABLES\033[0m\n{tab(6)}   \033[1mQUIZ\033[0m\n\nChoose a mode:  ")
     if mode.lower() == 'q':
         sys.exit("You have ended the program.")
     else:
@@ -74,15 +79,18 @@ while start_mode == True:
     if quiz == True:
         print("You have selected study mode.\nEnter 'q' to quit anytime.")
         print("\nSelect from the following modes:")
-        print("Mode 1: The multiplier remains the same for each question. \nOnly the multiplicand changes for each question.")
-        print("\nMode 2: Both the multiplier and the multiplicand can change for each question (harder mode!)")
+        print("\033[1mMode 1\033[0m: The multiplier remains the same for each question. \nOnly the multiplicand changes for each question.")
+        print("\n\033[1mMode 2\033[0m: Both the multiplier and the multiplicand can change for each question (harder mode!)")
 
     # Quiz Mode
     while quiz == True:
         quiz_mode1 = False
         quiz_mode2 = False
+        score = 0
+        streak = 0
+        hearts = 3
 
-        quiz_mode = input(f"\n{tab(5)}      Available Modes:\n{tab(6)}   MODE 1\n{tab(6)}   MODE 2\n\nChoose a mode:  ")
+        quiz_mode = input(f"\n{tab(5)}      Available Modes:\n{tab(6)}   \033[1mMODE 1\033[0m\n{tab(6)}   \033[1mMODE 2\033[0m\n\nChoose a mode:  ")
         if quiz_mode.lower() == 'q':
             break
         else:
@@ -106,23 +114,27 @@ while start_mode == True:
             elif hearts_on.lower() == "n":
                 hearts_on = False
             else:
-                hearts_on = False
                 continue 
         # Quiz Mode 1
         while quiz_mode1 == True: 
             multiplier = input("\n\nWhat number do you want to multiply by?  ")
-            if multiplier.lower() == 'q':
-                break
             while multiplier.isnumeric() == False:
+                if multiplier.lower() == 'q':
+                    break
                 print("Please enter a number.")
                 multiplier = input("\n\nWhat number do you want to multiply by?  ")
+            if multiplier.lower() == 'q':
+                    break
             multiplier = int(multiplier)
+
             multiplicand = input("\nWhat is the max number you want to multiply by?  ")
-            if multiplicand.lower() == 'q':
-                break
             while multiplicand.isnumeric() == False:
+                if multiplicand.lower() == 'q':
+                    break
                 print("Please enter a number.")
                 multiplicand = input("\nWhat is the max number you want to multiply by?  ")
+            if multiplicand.lower() == 'q':
+                break
             multiplicand = int(multiplicand)
 
             while answer_mode == True:
@@ -131,21 +143,25 @@ while start_mode == True:
                 answer = (input(f"{multiplier} x {temporary_multiplicand} = "))
                 if answer == "q":
                     hearts = 3
+                    score = 0
                     streak = 0
                     break
                 elif answer == str(product):
-                    print("Correct!\n\n")
+                    print("Correct! ✓\n\n")
+                    score += 1
                     streak += 1
+                    print(f"Score: {score}")
                     print(f"Streak: {streak}")
                     if hearts_on == True:
-                        print(f"Hearts remaining: {hearts}")
+                        hearts_count(hearts)
                 else:
-                    print(f"Wrong.\nAnswer: {product}\n")
+                    print(f"Wrong. ❌\nAnswer: {product}\n")
                     streak = 0
+                    print(f"Score: {score}")
                     print(f"Streak: {streak}")
                     if hearts_on == True:
                         hearts -= 1
-                        print(f"Hearts remaining: {hearts}")
+                        hearts_count(hearts)
                         if hearts == 0:
                             hearts = 3
                             status = input("Quiz over. Restart? (y/n)  ")
@@ -162,23 +178,27 @@ while start_mode == True:
             elif hearts_on.lower() == "n":
                 hearts_on = False
             else:
-                hearts_on = False
                 continue 
         # Quiz Mode 2
         while quiz_mode2 == True: 
             multiplier = input("\n\nSelect range for the first number:  ")
-            if multiplier.lower() == 'q':
-                break
             while multiplier.isnumeric() == False:
+                if multiplier.lower() == 'q':
+                    break
                 print("Please enter a number.")
                 multiplier = input("\n\nSelect range for the first number:  ")
+            if multiplier.lower() == 'q':
+                    break
             multiplier = int(multiplier)
+
             multiplicand = input("\nSelect range for the second number:  ")
-            if multiplicand.lower() == 'q':
-                break
             while multiplicand.isnumeric() == False:
+                if multiplicand.lower() == 'q':
+                    break
                 print("Please enter a number.")
                 multiplicand = input("\nSelect range for the second number:  ")
+            if multiplicand.lower() == 'q':
+                break
             multiplicand = int(multiplicand)
 
             while answer_mode == True:
@@ -188,21 +208,25 @@ while start_mode == True:
                 answer = (input(f"{temporary_multiplier} x {temporary_multiplicand} = "))
                 if answer == "q":
                     hearts = 3
+                    score = 0
                     streak = 0
                     break
                 elif answer == str(product):
-                    print("Correct!\n\n")
+                    print("Correct! ✓\n\n")
+                    score += 1
                     streak += 1
+                    print(f"Score: {score}")
                     print(f"Streak: {streak}")
                     if hearts_on == True:
-                        print(f"Hearts remaining: {hearts}")
+                        hearts_count(hearts)
                 else:
-                    print(f"Wrong.\nAnswer: {product}\n")
+                    print(f"Wrong. ❌\nAnswer: {product}\n")
                     streak = 0
+                    print(f"Score: {score}")
                     print(f"Streak: {streak}")
                     if hearts_on == True:
                         hearts -= 1
-                        print(f"Hearts remaining: {hearts}")
+                        hearts_count(hearts)
                         if hearts == 0:
                             hearts = 3
                             status = input("Quiz over. Restart? (y/n)  ")
